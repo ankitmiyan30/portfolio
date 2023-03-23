904c92d271d678dd25ee2f506c83a4e105cdf117
ankit.miyan <ankit.miyan@techspian.com>
2023-03-23 14:28:15 +0530

    Initial Test Case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,20 +12,5 @@ import 'package:my_portfolio/main.dart';
 import 'package:my_portfolio/widget/home.dart';
 
 void main() {
-  testWidgets('', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomePage());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  testWidgets('', (WidgetTester tester) async {});
 }
